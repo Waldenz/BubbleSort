80ca1f5e9c27c6899cc743f5a711627f245eabd9
waldenz <waldenzww@gmail.com>
2013-09-25 14:09:24 +0800

    comm

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BubbleSort
+{
+    class BubbleSort<T> where T : IComparable
+    {
+        public void BubbleSortFunction(T[] array)
+        {
+            foreach (T a in array)
+            {
+                Console.Write(a.ToString() + " ");
+            }
+            Console.WriteLine();
+
+            int length = array.Length;
+            T temp;
+
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = length - 1; j > i; j--)
+                {
+                    if (array[j].CompareTo(array[j - 1]) < 0)
+                    {
+                        temp = array[j];
+                        array[j] = array[j - 1];
+                        array[j - 1] = temp;
+                    }
+                }
+
+            }
+
+            foreach (T a in array)
+            {
+                Console.Write(a.ToString() + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ BubbleSort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort<string> bsStr = new BubbleSort<string>();
+            string[] strArr = { "c", "b", "a", "d", "f", "z" };
+            bsStr.BubbleSortFunction(strArr);
+
+            BubbleSort<int> bsInt = new BubbleSort<int>();
+            int[] intArr = new int[] { 1, 9, 5, 8, 2, 7 };
+            bsInt.BubbleSortFunction(intArr);
+
+            Console.WriteLine("Success!");
+            Console.ReadLine();
+        }
+    }
+}
